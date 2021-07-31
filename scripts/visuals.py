e20807700a0c9fdfca431c24a8fc415de0d36f26
RyanCHolder <86632015+RyanCHolder@users.noreply.github.com>
2021-07-31 09:43:15 -0700

    Update visuals.py

--- scripts/visuals.py
+++ scripts/visuals.py
@@ -3,10 +3,11 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 #directory for the original data
-origin_dir = 'WESAD/'
+WESAD_dir = 'WESAD/'
 
-#directory for statistical formatted data
-reg_dir = 'Data/Regular/'
+#directory for formatted data
+form_WESAD = 'Formatted_WESAD/Raw/'
+form_ADARP = 'Formatted_ADARP/Raw/'
 
 def load(path):
     """
@@ -26,17 +27,19 @@ def plot_data(tag, data, labels):
         @param: labels (list): Data labels (kept separate because of differing
             organization in formatted and unformatted data)
     """
-
+    #set font
+    plt.rcParams.update({'font.size': 22})
     #plot labels
     plt.figure(1)
     plt.plot(range(len(labels)), labels)
     plt.ylabel("Label")
-    plt.title(tag + " Labels")
+    plt.xlabel("Samples Over Time")
+    #plt.title(tag + " Label Distribution")
 
     #plot acceleration
     plt.figure(2)
     plt.subplot(3,1,1)
-    plt.title(tag + " Acceleration Data")
+    #plt.title(tag + " Acceleration")
     plt.plot(range(len(data['ACC'])),data['ACC'][:,0])
     plt.ylabel("X Acceleration")
     plt.subplot(3,1,2)
@@ -45,24 +48,28 @@ def plot_data(tag, data, labels):
     plt.subplot(3,1,3)
     plt.plot(range(len(data['ACC'])),data['ACC'][:,2])
     plt.ylabel("Z Acceleration")
+    plt.xlabel("Samples Over Time")
 
     #plot BVP
     plt.figure(3)
-    plt.title(tag + " Blood Volume Pulse Data")
+    #plt.title(tag + " Blood Volume Pulse")
     plt.plot(range(len(data['BVP'])),data['BVP'])
     plt.ylabel("Blood Volume Pulse")
+    plt.xlabel("Samples Over Time")
 
     #plot EDA
     plt.figure(4)
-    plt.title(tag + " Electrodermal Activity")
+    #plt.title(tag + " Electrodermal Activity")
     plt.plot(range(len(data['EDA'])),data['EDA'])
     plt.ylabel("Electrodermal Activity")
+    plt.xlabel("Samples Over Time")
 
     #plot TEMP
     plt.figure(5)
-    plt.title(tag + " Temperature Data")
+    #plt.title(tag + " Temperature")
     plt.plot(range(len(data['TEMP'])),data['TEMP'])
     plt.ylabel("Temperature")
+    plt.xlabel("Samples Over Time")
 
     plt.show()
 
@@ -72,22 +79,23 @@ def plot_unformatted(subject):
         @param: subject (String): Number corresponding to the subject to graph
     """
     #gather data
-    data = load(origin_dir + 'S' + subject + '/S' + subject + '.pkl')
+    data = load(WESAD_dir + 'S' + subject + '/S' + subject + '.pkl')
     labels = data['label']
     data = data['signal']['wrist']
     #plot data
-    plot_data("Unformatted Subject " + subject, data, labels)
+    plot_data("Unformmated Subject " + subject, data, labels)
 
-def plot_formatted(subject):
+def plot_formatted(subject, dir):
     """
-        @breif: plots the formatted WESAD data (the mean of each timestep is
+        @breif: plots the formatted data (the mean of each timestep is
         used to keep the data in 2 dimensions for plotting)
         @param: subject (String): Number corresponding to the subject to graph
+        @param: dir (String): Location of data
 
-        Note: this data is normalized, so the y-values are only relative
+        Note:6 this data is normalized, so the y-values are only relative
     """
     #gather data
-    data = load(reg_dir + 'S' + subject + '.pkl')
+    data = load(dir + 'S' + subject + '.pkl')
 
     #take averages of each timestep
     for feat in data['data']:
@@ -95,5 +103,3 @@ def plot_formatted(subject):
 
     #plot data
     plot_data("Formatted Subject " + subject, data['data'], data['labels'])
-
-plot_formatted('2')
